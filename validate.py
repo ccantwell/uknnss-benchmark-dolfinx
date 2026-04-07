b9db243691bedcbf62f353c7473c4a9d3e33c1a0
Chris Cantwell <c.cantwell@imperial.ac.uk>
2026-04-07 17:59:56 +0100

    Improved validation script.

--- validate.py
+++ validate.py
@@ -3,59 +3,97 @@
 import json
 import sys
 
-if len(sys.argv) != 2:
-    print("validate.py: test output correctness and extract performance for the UK-NNSS Grid benchmark.")
-    print("Usage: validate.py <result_json_file>")
+
+# Parse console output
+def parse_output( json_fname, console_fname ):
+    # Create dictionary for parsed values 
+    d = dict()
+
+    # Parse console file
+    d['re'] = None
+    f = open( console_fname, 'r' )
+    for l in f:
+        if("Relative norm of error = " in l ):
+            s = l.split()
+            d['re'] = float(s[5])
+    f.close()
+ 
+    # Parse JSON output file
+    with open(json_fname, 'r') as file:
+        data = json.load(file)
+    # Get the inputs
+    d['p'] = data['input'].get('p', None)
+    d['ndof'] = data['input'].get('ndofs_local_requested', None)
+    d['nreps'] = data['input'].get('nreps', None)
+    d['fp'] = data['input'].get('scalar_size', None)
+    d['is_mat_comp'] = data['input'].get('mat_comp', False)
+    d['is_cg'] = data['input'].get('cg', False)
+    d['gdofs'] = data['output'].get('gdof_per_second', None)
+    d['y_norm'] = data['output'].get('y_norm', None)
+    d['z_norm'] = data['output'].get('z_norm', None)
+
+    return d
+
+# ------------------------------------
+
+if len(sys.argv) != 3:
+    print("validate.py: test output correctness and extract performance for the UK-NNSS DOLFINx benchmark.")
+    print("Usage: validate.py <result_json_file> <console_output_file>")
     sys.exit(1)
 
 
 jsonfile = sys.argv[1]
+consolefile = sys.argv[2]
 
-with open(jsonfile, 'r') as file:
-    data = json.load(file)
+d = parse_output(jsonfile, consolefile)
 
 valid = True
 
 print("\n# DOLFINx benchmark validation\n")
 
-# Get the inputs
-p = data['input']['p']
-ndof = data['input']['ndofs_local_requested']
-nreps = data['input']['nreps']
-fp = data['input']['scalar_size']
-is_mat_comp = data['input']['mat_comp']
-is_cg = data['input']['cg']
-print(f'                   P : {p}')
-print(f'                ndof : {ndof}')
-print(f'               nreps : {nreps}')
-print(f'         scalar size : {fp}')
+print(f'                   P : {d["p"]}')
+print(f'                ndof : {d["ndof"]}')
+print(f'               nreps : {d["nreps"]}')
+print(f'         scalar size : {d["fp"]}')
 
-if not is_mat_comp and not is_cg:
-    print(f'\n  Benchmark must be run with exactly one of --mat_comp and --cg')
+if not d["is_mat_comp"] and not d["is_cg"]:
+    print(f'\n  ERROR: Benchmark must be run with exactly one of --mat_comp and --cg')
     valid = False
 
-if fp != 64:
-    print(f'\n  Benchmark must be run with 64-bit precision')
+if d["fp"] != 64:
+    print(f'\n  ERROR: Benchmark must be run with 64-bit precision')
     valid = False
 
-if valid and ndof == 10000 and p == 3:
-    print(f'\n  ===========================================')
-    print(f'  CORRECTNESS TEST CASE                      ')
-    y_norm = data['output']['y_norm']
-    z_norm = data['output']['z_norm']
-    print(f'              y_norm : {y_norm}')
-    print(f'              z_norm : {z_norm}')
-    if abs(y_norm - 1.141577508) > 1e-09 or abs(z_norm - 1.141577508) > 1e-09:
-        print(f'  INCORRECT VALUES REPORTED                  ')
+if d["nreps"] < 100:
+    print(f'\n  ERROR: nreps too small, please run with at least 100')
+    valid = False
+
+if d["is_mat_comp"]:
+    if d['re'] is None:
+        print("Error: could not find relaive norm of error in {:}".format(
+            consolefile ) )
+        print("       should have found \"Relative norm of error =  \"" )
         valid = False
-    print(f'  ===========================================')
+    
+    if valid and d["ndof"] == 10000 and d["p"] == 3:
+        print(f'\n  ===========================================')
+        print(f'  CORRECTNESS TEST CASE                      ')
+        print(f'              y_norm : {d["y_norm"]}')
+        print(f'              z_norm : {d["z_norm"]}')
+        if abs(d["y_norm"] - 1.141577508) > 1e-09 or abs(d["z_norm"] - 1.141577508) > 1e-09:
+            print(f'  ERROR: INCORRECT VALUES REPORTED                  ')
+            valid = False
+        print(f'  ===========================================')
 
-gdofs = data['output']['gdof_per_second']
+    if valid and abs(d["re"]) > 1e-14:
+        print(f'   Relative error norm too large')
+        valid = False
 
-if valid and is_mat_comp:
-    print(f'   MAT COMP performance: {gdofs} Gflops/s')
-if valid and is_cg:
-    print(f'   CG performance: {gdofs} Gflops/s')
+    if valid:
+        print(f'\n  MAT COMP performance: {d["gdofs"]} Gflops/s')
+
+if d["is_cg"] and valid:
+    print(f'\n  CG performance: {d["gdofs"]} Gflops/s')
 
 print("\n  Validation:", ("PASSED" if valid else "FAILED") )
 print()
